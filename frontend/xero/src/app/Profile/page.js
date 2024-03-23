9a106ab7cafe07d0ff74e23bed1c5df5aa19801f
name <yourgithub@email.com>
2024-03-23 23:26:12 +0530

    Qoestion

---
+++ frontend/xero/src/app/Profile/page.js
@@ -0,0 +1,40 @@
+"use client";
+import React from "react";
+import styles from "./index.module.css";
+import Image from 'next/image'; // Uncomment this line to import the Image component
+
+const Profile = () => {
+  return (
+    <>
+    <div className={styles.main}>
+    <div className={styles.Div}>
+    <div className={styles.Div2}>Health Profile Setup
+    <div className={styles.Div3} />
+    </div>
+  </div>
+  <div className={styles.above}>
+    <div className={styles.img}>
+    <Image
+        src="/dummy.png"
+        alt="Description of the image"
+        width={50} // Width of the image
+        height={50} // Height of the image
+      />
+    </div>
+    <div className={styles.name}>
+        Jay Sharma
+   
+    <div className={styles.age}>
+        25
+        </div>
+    </div>
+  </div>
+<div className={styles.comp1}>
+    
+</div>
+
+    </div>
+    </>
+  );
+};
+export default Profile;
