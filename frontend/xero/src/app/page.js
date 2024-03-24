91dd84ce8d976462cf3957fbfcc11acfe706fb9f
name <yourgithub@email.com>
2024-03-24 08:50:52 +0530

    ml model and sos

--- frontend/xero/src/app/page.js
+++ frontend/xero/src/app/page.js
@@ -1,6 +1,6 @@
 "use client"
-
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
